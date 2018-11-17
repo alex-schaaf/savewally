ba98713d6c3af31f2f283d51c26dccc104d34cda
bomtuckle <trb.buckle@gmail.com>
2018-11-17 16:50:20 +0000

    Update __init__.py

--- savewally/__init__.py
+++ savewally/__init__.py
@@ -28,3 +28,18 @@ def axisTile (Coord, depth):
 
 def find_nearest(array, value):
    return (np.abs(array - value)).argmin()
+
+
+def non_white(mask_array, level=8):
+    """
+    Returns list of paths to images with any non white values
+    """
+    image_paths = []
+    for i in range(256):
+        for j in range(256):
+            if mask_array[-i, j]:
+                image_path = "Stitch_3/{}".format(sw.GetFilepath(col=i, row=j, k=level))
+            else:
+                continue
+            image_paths.append(image_path)
+    return image_paths
